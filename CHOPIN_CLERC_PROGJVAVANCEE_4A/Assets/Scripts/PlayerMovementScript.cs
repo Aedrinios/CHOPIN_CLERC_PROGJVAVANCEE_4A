72ddc49a5ea0286c656c6b25500cbc6fd83e51b4
Clément Clerc <clementclercam@gmail.com>
2020-09-28 19:52:44 +0200

    Character Controller WIP
    Simple movement (left, right and jump) & Arena prototype

---
+++ CHOPIN_CLERC_PROGJVAVANCEE_4A/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller;
+
+    [Header("Player Physics Attributes")]
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity = -9.81f;
+
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * -gravity);
+        }
+
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * x;
+        controller.Move(move * speed * Time.deltaTime);
+        velocity.y += -gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
