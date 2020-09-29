8d310d0cfda915ef9d30185c4b3616c3b8ccf614
Aedrinios <clementclerc2@gmail.com>
2020-09-29 09:38:10 +0200

    Add BallControllerScript

---
+++ CHOPIN_CLERC_PROGJVAVANCEE_4A/Assets/Scripts/BallControllerScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControllerScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    
+    [SerializeField]
+    private LayerMask playerMask;
+
+    [SerializeField]
+    private LayerMask wallMask;
+
+    private Vector3 direction = Vector3.forward;
+
+    private void Start()
+    {
+        float xDirection = UnityEngine.Random.Range(-1.0f, 1.0f); 
+        float zDirection = UnityEngine.Random.Range(0.0f, 1.0f);
+        direction = new Vector3(xDirection, 0.0f, zDirection);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position += direction.normalized * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ((wallMask.value & (1 << collision.gameObject.layer)) > 0)
+            direction = new Vector3(-direction.x, direction.y, direction.z);
+
+        else if ((playerMask.value & (1 << collision.gameObject.layer)) > 0)
+            direction = new Vector3(direction.x, direction.y, -direction.z);
+    }
+}
