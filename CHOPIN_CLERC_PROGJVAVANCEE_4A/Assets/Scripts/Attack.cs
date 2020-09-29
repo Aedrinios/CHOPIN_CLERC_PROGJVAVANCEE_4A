b3683bdcc6d478db56d0487bdafa2b82cf58d85f
cedricChopin <72026207+cedricChopin@users.noreply.github.com>
2020-09-29 08:29:37 +0200

    Modif project
    Improving movement and jump

---
+++ CHOPIN_CLERC_PROGJVAVANCEE_4A/Assets/Scripts/Attack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private int _attack = 6;
+    private int _defense = 6;
+    private int _maximumHealth = 100;
+    private int _maxMP = 100;
+    private int _luck = 6;
+    private int _int = 6;
+    private int hitRange = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Attacks()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 origin = transform.position;
+
+
+     if (Physics.Raycast(origin, forward, out hit))
+        {
+            if (hit.transform.gameObject.tag == "Enemy")
+            {
+                Debug.Log("Ennemy is getting hit");
+            }
+        }
+    }
+}
---
+++ CHOPIN_CLERC_PROGJVAVANCEE_4A/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField]
+    private float playerSpeed = 9.0f; // Vitesse du Player
+    [SerializeField]
+    private float jumpHeight = 4.0f; // Puissance de saut
+    [SerializeField]
+    private float gravityValue = -9.81f;
+    public LayerMask groundMask;
+    public string axis;
+    public string Button_jump;
+
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Test si le Player est au sol
+        groundedPlayer = controller.isGrounded; 
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+        // Mouvement Horizontal du Player
+        Vector3 move = new Vector3(Input.GetAxis(axis), 0, 0);
+        controller.Move(move * Time.deltaTime * playerSpeed); 
+
+
+        // Application de la valeur pour le jump
+        if (Input.GetButton(Button_jump) && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue); 
+        }
+        // Jump du Player
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime); 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!controller.isGrounded)
+        {
+            playerVelocity.y -= gravityValue * Time.deltaTime;
+
+        }
+    }
+}
