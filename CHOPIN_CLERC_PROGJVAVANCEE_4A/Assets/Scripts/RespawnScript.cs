f1258404669921fb3c280e7897d3455a64cf20d1
cedricChopin <72026207+cedricChopin@users.noreply.github.com>
2020-09-29 11:15:20 +0200

    RespawnScript

---
+++ CHOPIN_CLERC_PROGJVAVANCEE_4A/Assets/Scripts/RespawnScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnScript : MonoBehaviour
+{
+    public Transform ZoneStart;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = ZoneStart.transform.position;
+        }
+    }
+}
