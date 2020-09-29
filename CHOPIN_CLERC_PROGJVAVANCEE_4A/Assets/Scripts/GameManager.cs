a82249bac84ffd6c339b8f8733b3165c7e28542e
Aedrinios <clementclerc2@gmail.com>
2020-09-29 12:02:02 +0200

    Import TextMeshPro + UI Scene WIP

---
+++ CHOPIN_CLERC_PROGJVAVANCEE_4A/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
