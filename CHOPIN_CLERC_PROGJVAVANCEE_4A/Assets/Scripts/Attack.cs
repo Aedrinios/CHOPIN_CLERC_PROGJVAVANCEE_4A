39f8611c905ebf360e1e2a6bc9817dce111b9fab
cedricChopin <72026207+cedricChopin@users.noreply.github.com>
2020-09-30 08:44:39 +0200

    Reflect ball

--- CHOPIN_CLERC_PROGJVAVANCEE_4A/Assets/Scripts/Attack.cs
+++ CHOPIN_CLERC_PROGJVAVANCEE_4A/Assets/Scripts/Attack.cs
@@ -1,81 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
 
     private RaycastHit hit;
-    public Transform holdPoint;
+    public CharacterController cc;
     private bool grabbed = false;
+    private Vector3 direction = Vector3.forward;
+
     // Start is called before the first frame update
     void Start()
     {
-        holdPoint = this.transform;
+        cc = this.GetComponent<CharacterController>();
     }
 
+    
     // Update is called once per frame
-    void Update()
+   
+
+    public void OnTriggerStay(Collider other)
     {
-        if (Input.GetButtonDown("Grab"))
+        if (other.gameObject.CompareTag("Ball"))
         {
-            Debug.Log("Attack");
-            Attacks();
-        }
-    }
-    void Attacks()
-    {
-        RaycastHit hit;
-        Vector3 forward = transform.TransformDirection(Vector3.right);
-        Vector3 origin = transform.position;
-
-
-        if (Physics.Raycast(origin, forward, out hit))
-        {
-            if (hit.transform.gameObject.tag == "Ball")
+            if (Input.GetButtonDown("Grab"))
             {
-               // hit.transform.gameObject.SetActive(false); // Test pour désactiver la boule quand ca touche
-                CmdGrabAndThrow();
-            }
+                BallControllerScript.Instance().getReflected() ;
+                Debug.Log("Attack");
 
-
-        }
-    }
-    public void CmdGrabAndThrow()
-
-    {
-        bool grabbed = false;
-        if (!grabbed)
-        {
-
-
-
-            if (hit.collider != null)
-            {
-                grabbed = true;
-            }
-
-            else
-            {
-                grabbed = false;
-
-                hit.collider.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(transform.localScale.x, 1f, 0.0f);
-
-            }
-
-        }
-        if (grabbed)
-        {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                grabbed = false;
-                hit.collider.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(transform.localScale.x * 5, 1f, 0f);
-            }
-            else
-            {
-                hit.collider.gameObject.transform.position = this.transform.position;
             }
         }
     }
+ 
 }
 
