bb539f0dfa21bc8c8d2325c610d6fe2f36fc6bfe
Aedrinios <clementclerc2@gmail.com>
2020-10-02 17:04:09 +0200

    correction

--- CHOPIN_CLERC_PROGJVAVANCEE_4A/Assets/Scripts/Entities/Player/PlayerControllerScript.cs
+++ CHOPIN_CLERC_PROGJVAVANCEE_4A/Assets/Scripts/Entities/Player/PlayerControllerScript.cs
@@ -72,7 +72,6 @@ public class PlayerControllerScript : MovingEntityScript
                 animator.SetBool("IsRunning", true);
                 animator.SetBool("IsJumping", false);
                 animator.SetBool("IsJumping", false);
-                animationCount = 0;
             }
             else
             {
@@ -142,7 +141,6 @@ public class PlayerControllerScript : MovingEntityScript
             rotation.y = 180;
 
         body.rotation = rotation;
-        animationCount += Time.deltaTime;
         playerVelocity = transform.TransformDirection(playerVelocity);
         playerVelocity *= speed;
 
